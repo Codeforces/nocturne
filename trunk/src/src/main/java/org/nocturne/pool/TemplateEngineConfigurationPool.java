5101dceb83fe8151dc497b94d45b3bd2736eef8d
mirzayanovmr <mirzayanovmr@65389464-0340-11de-9fdb-3debd5008f28>
2009-03-13 09:51:17 +0000

    Supported custom template loading: use nocturne.template-loader-class in web.xml to specify custom template loader class.

--- trunk/src/src/main/java/org/nocturne/pool/TemplateEngineConfigurationPool.java
+++ trunk/src/src/main/java/org/nocturne/pool/TemplateEngineConfigurationPool.java
@@ -2,14 +2,20 @@ package org.nocturne.pool;
 
 import freemarker.template.Configuration;
 import freemarker.template.DefaultObjectWrapper;
+import freemarker.cache.TemplateLoader;
+import freemarker.cache.FileTemplateLoader;
 import org.nocturne.misc.ApplicationContext;
 
 import javax.servlet.FilterConfig;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 /** @author Mike Mirzayanov */
 public class TemplateEngineConfigurationPool extends Pool<Configuration> {
+    private static final String TEMPLATE_LOADER_CLASS_PARAMETER_KEY = "nocturne.template-loader-class";
+
     private final ApplicationContext applicationContext;
     private final FilterConfig filterConfig;
 
@@ -29,12 +35,52 @@ public class TemplateEngineConfigurationPool extends Pool<Configuration> {
 
             Configuration templateEngineConfiguration = new Configuration();
             templateEngineConfiguration.setDirectoryForTemplateLoading(
-                directory
+                    directory
             );
+
+            setupTemplateLoaderClass(templateEngineConfiguration, directory);
             templateEngineConfiguration.setObjectWrapper(new DefaultObjectWrapper());
             return templateEngineConfiguration;
         } catch (IOException e) {
             throw new IllegalStateException("Can't create template engine.", e);
+        }
+    }
+
+    @SuppressWarnings({"unchecked"})
+    private void setupTemplateLoaderClass(Configuration templateEngineConfiguration, File templateDir) {
+        String templateLoaderClassParam = filterConfig.getInitParameter(TEMPLATE_LOADER_CLASS_PARAMETER_KEY);
+
+        if (templateLoaderClassParam != null) {
+            try {
+                Class<? extends TemplateLoader> templateLoaderClass =
+                        (Class<? extends TemplateLoader>) TemplateEngineConfigurationPool.class.getClassLoader().loadClass(templateLoaderClassParam);
+
+                TemplateLoader loader;
+                if (FileTemplateLoader.class.isAssignableFrom(templateLoaderClass)) {
+                    try {
+                        Constructor<? extends TemplateLoader> fileConstructor = templateLoaderClass.getConstructor(File.class);
+                        loader = fileConstructor.newInstance(templateDir);
+                    } catch (NoSuchMethodException e) {
+                        throw new IllegalStateException("Expected constructor from File for FileTemplateLoader " +
+                                "and derived classes [class=" + templateLoaderClass.getName() + "].", e);
+                    } catch (InvocationTargetException e) {
+                        throw new IllegalStateException("Can't invoke constructor from File for FileTemplateLoader " +
+                                "or derived class [class=" + templateLoaderClass.getName() + "].", e);
+                    }
+                } else {
+                    loader = templateLoaderClass.newInstance();
+                }
+                templateEngineConfiguration.setTemplateLoader(loader);
+            } catch (ClassNotFoundException e) {
+                throw new IllegalStateException("Can't find template loader class "
+                        + templateLoaderClassParam + ".", e);
+            } catch (IllegalAccessException e) {
+                throw new IllegalStateException("Not enough access to instantiate template loader class "
+                        + templateLoaderClassParam + ".", e);
+            } catch (InstantiationException e) {
+                throw new IllegalStateException("Can't instantiate template loader class "
+                        + templateLoaderClassParam + ".", e);
+            }
         }
     }
 }
