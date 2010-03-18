30c2e8950691d057752399d3c5a36054bae15fb3
mirzayanovmr <mirzayanovmr@65389464-0340-11de-9fdb-3debd5008f28>
2010-03-18 09:09:58 +0000

    Added missed package

---
+++ trunk/code/src/main/java/org/nocturne/cache/CacheHandler.java
@@ -0,0 +1,22 @@
+package org.nocturne.cache;
+
+import org.nocturne.main.Component;
+
+/**
+ * Use implementation of this interface in your pages or frames
+ * if you want them to be cached.
+ *
+ * Method #intercept will be called after Component.prepareForAction
+ * and if it returns non-null string it will be used as parsed result instead
+ * of typical life-cycle, i.e. methods initializeAction(), Events.fireBeforeAction(this),
+ * ..., Events.fireAfterAction(this), finalizeAction()) will not be called.
+ *
+ * But if it returns null, the typical life-cycle will be used and parsed component
+ * will be passed as a result to #postprocess().
+ *
+ * @author Mike Mirzayanov (mirzayanovmr@gmail.com)
+ */
+public interface CacheHandler {
+    String intercept(Component component);
+    void postprocess(Component component, String result);
+}
