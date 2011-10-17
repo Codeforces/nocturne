290e21ff605e2eef8e6692aa85734387d6dd04ca
sladethe <sladethe@65389464-0340-11de-9fdb-3debd5008f28>
2011-10-17 07:36:11 +0000

    - increased versions of some project dependencies; - improved LinkDirective: now we can add interceptors to postprocess links.

--- code/src/main/java/org/nocturne/link/LinkDirective.java
+++ code/src/main/java/org/nocturne/link/LinkDirective.java
@@ -1,14 +1,15 @@
 /*
  * Copyright 2009 Mike Mirzayanov
  */
-
 package org.nocturne.link;
 
 import freemarker.core.Environment;
 import freemarker.template.*;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Generates page link.
@@ -16,11 +17,13 @@ import java.util.Map;
  * @author Mike Mirzayanov
  */
 public class LinkDirective implements TemplateDirectiveModel {
-    /** Singleton instance. */
-    private static LinkDirective INSTANCE = new LinkDirective();
+    private static final LinkDirective INSTANCE = new LinkDirective();
 
-    @SuppressWarnings({"unchecked"})
-    public void execute(Environment env, Map params, TemplateModel[] loopVars, TemplateDirectiveBody body) throws TemplateException, IOException {
+    private final List<Interceptor> interceptors = new CopyOnWriteArrayList<Interceptor>();
+
+    @Override
+    public void execute(Environment env, Map params, TemplateModel[] loopVars, TemplateDirectiveBody body)
+            throws TemplateException, IOException {
         if (!params.containsKey("name")) {
             throw new TemplateModelException("LinkDirective directive expects name parameter.");
         }
@@ -32,22 +35,58 @@ public class LinkDirective implements TemplateDirectiveModel {
             throw new TemplateModelException("LinkDirective directive doesn't allow loop variables.");
         }
 
-        if (body != null) {
-            throw new TemplateModelException("Body is not expected for LinkDirective directive.");
-        } else {
+        if (body == null) {
             if (params.containsKey("value")) {
                 String value = params.get("value").toString();
                 params.remove("value");
-                String a = String.format("<a href=\"%s\">%s</a>", Links.getLinkByMap(name, params), value);
+                String a = String.format("<a href=\"%s\">%s</a>", getLink(params, name), value);
                 env.getOut().write(a);
             } else {
-                env.getOut().write(Links.getLinkByMap(name, params));
+                env.getOut().write(getLink(params, name));
             }
+        } else {
+            throw new TemplateModelException("Body is not expected for LinkDirective directive.");
         }
     }
 
-    /** @return Returns the only directive instance. */
+    private String getLink(Map params, String name) {
+        //noinspection unchecked
+        String link = Links.getLinkByMap(name, params);
+
+        for (Interceptor interceptor : interceptors) {
+            link = interceptor.process(link, name, params);
+        }
+
+        return link;
+    }
+
+    /**
+     * Adds interceptor to the link directive.
+     * Link will be processed by interceptors after all logic of this directive is completed
+     * and before resulting string will be written into the output.
+     *
+     * @param interceptor link directive interceptor to add
+     */
+    public void addInterceptor(Interceptor interceptor) {
+        interceptors.add(interceptor);
+    }
+
+    /**
+     * @return Returns the only directive instance.
+     */
     public static LinkDirective getInstance() {
         return INSTANCE;
+    }
+
+    public interface Interceptor {
+        /**
+         * Link directive calls this method to postprocess link.
+         *
+         * @param link   link to process
+         * @param name   name of the link
+         * @param params parameters of the link
+         * @return processed link
+         */
+        String process(String link, String name, Map params);
     }
 }
