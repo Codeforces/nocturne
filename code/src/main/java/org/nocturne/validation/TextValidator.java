ce419e3c390f5285cb426e84f4b3cabdca5532ad
neddi.homosapiens <neddi.homosapiens@gmail.com>
2012-06-07 10:40:48 +0000

    - TextValidator with specific message (with parameters for caption).

--- code/src/main/java/org/nocturne/validation/TextValidator.java
+++ code/src/main/java/org/nocturne/validation/TextValidator.java
@@ -6,11 +6,38 @@ package org.nocturne.validation;
  * @author Mike Mirzayanov
  */
 public class TextValidator extends Validator {
+    /**
+     * Shortcut for error message.
+     */
+    private String message = "Field can't contain binary data";
+
+    /**
+     * Parameters for message caption.
+     */
+    private Object[] messageParams = { };
+
+    /**
+     * Creates validator with default error message: Field can't contain binary data.
+     */
+    public TextValidator() {
+    }
+
+    /**
+     * Creates validator with specific error message.
+     * Actually caption shortcut should be passed.
+     *
+     * @param message Error message caption shortcut.
+     */
+    public TextValidator(String message, Object... messageParams) {
+        this.message = message;
+        this.messageParams = messageParams;
+    }
+
     @Override
     public void run(String value) throws ValidationException {
         for (int i = 0; i < value.length(); i++) {
             if (value.charAt(i) < 9) {
-                throw new ValidationException($("Field can't contain binary data"));
+                throw new ValidationException($(message, messageParams));
             }
         }
     }
