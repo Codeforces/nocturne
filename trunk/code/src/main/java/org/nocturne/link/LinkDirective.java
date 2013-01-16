413094d4e5538fa81b59384a4195ec610efbd142
sladethe <sladethe@65389464-0340-11de-9fdb-3debd5008f28>
2013-01-16 08:14:48 +0000

    - supported 'linkName' parameter in LinkDirective.

--- trunk/code/src/main/java/org/nocturne/link/LinkDirective.java
+++ trunk/code/src/main/java/org/nocturne/link/LinkDirective.java
@@ -14,8 +14,13 @@ import java.util.Map;
  *
  * @author Mike Mirzayanov
  */
+@SuppressWarnings("Singleton")
 public class LinkDirective implements TemplateDirectiveModel {
     private static final LinkDirective INSTANCE = new LinkDirective();
+
+    private LinkDirective() {
+        // No operations.
+    }
 
     @Override
     public void execute(Environment env, Map params, TemplateModel[] loopVars, TemplateDirectiveBody body)
@@ -24,21 +29,23 @@ public class LinkDirective implements TemplateDirectiveModel {
             throw new TemplateModelException("LinkDirective directive expects name parameter.");
         }
 
-        String name = params.get("name").toString();
-        params.remove("name");
+        String name = params.remove("name").toString();
 
         if (loopVars.length != 0) {
             throw new TemplateModelException("LinkDirective directive doesn't allow loop variables.");
         }
 
+        Object linkNameValue = params.remove("linkName");
+        String linkName = linkNameValue == null ? null : linkNameValue.toString();
+
         if (body == null) {
             if (params.containsKey("value")) {
                 String value = params.get("value").toString();
                 params.remove("value");
-                String a = String.format("<a href=\"%s\">%s</a>", getLink(params, name), value);
+                String a = String.format("<a href=\"%s\">%s</a>", getLink(params, name, linkName), value);
                 env.getOut().write(a);
             } else {
-                env.getOut().write(getLink(params, name));
+                env.getOut().write(getLink(params, name, linkName));
             }
         } else {
             throw new TemplateModelException("Body is not expected for LinkDirective directive.");
@@ -46,8 +53,8 @@ public class LinkDirective implements TemplateDirectiveModel {
     }
 
     @SuppressWarnings("unchecked")
-    private static String getLink(Map params, String name) {
-        return Links.getLinkByMap(name, params);
+    private static String getLink(Map params, String name, String linkName) {
+        return linkName == null ? Links.getLinkByMap(name, params) : Links.getLinkByMap(name, linkName, params);
     }
 
     /**
