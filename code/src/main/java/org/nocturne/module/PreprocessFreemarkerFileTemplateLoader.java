e303775bad0d02150493cfc962726a71b6124d93
sladethe <sladethe@65389464-0340-11de-9fdb-3debd5008f28>
2013-05-30 10:28:03 +0000

    - improved template loading engine.

--- code/src/main/java/org/nocturne/module/PreprocessFreemarkerFileTemplateLoader.java
+++ code/src/main/java/org/nocturne/module/PreprocessFreemarkerFileTemplateLoader.java
@@ -6,54 +6,67 @@ package org.nocturne.module;
 import freemarker.cache.FileTemplateLoader;
 import org.nocturne.main.ApplicationContext;
 
-import java.io.CharArrayReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.StringReader;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Scans loaded templates to contains {{...}} and uses captions framework to
- * substitutes them to caption values.
+ * substitute them to caption values.
  *
  * @author Mike Mirzayanov
  */
 public class PreprocessFreemarkerFileTemplateLoader extends FileTemplateLoader {
+    private static final ConcurrentMap<String, InmemoryTemplateSource> templateSourceByName = new ConcurrentHashMap<String, InmemoryTemplateSource>();
+
     public PreprocessFreemarkerFileTemplateLoader(File baseDir) throws IOException {
         super(baseDir);
     }
 
     @Override
     public Object findTemplateSource(String name) throws IOException {
-        return super.findTemplateSource(name);
+        InmemoryTemplateSource templateSource = templateSourceByName.get(name);
+        return templateSource == null ? super.findTemplateSource(name) : templateSource;
     }
 
+    @SuppressWarnings("RefusedBequest")
     @Override
     public Reader getReader(Object templateSource, String encoding) throws IOException {
-        Reader reader = super.getReader(templateSource, encoding);
-        StringBuffer sb = new StringBuffer();
+        StringBuilder stringBuilder = getTemplateAsStringBuilder(templateSource, encoding);
+        processText(stringBuilder);
+        return new StringReader(stringBuilder.toString());
+    }
 
-        char[] buffer = new char[65536];
-        while (true) {
-            int readByteCount = reader.read(buffer);
-            if (readByteCount == -1) {
-                break;
+    private StringBuilder getTemplateAsStringBuilder(Object templateSource, String encoding) throws IOException {
+        if (templateSource instanceof InmemoryTemplateSource) {
+            return new StringBuilder(((InmemoryTemplateSource) templateSource).getContent());
+        } else {
+            Reader reader = super.getReader(templateSource, encoding);
+            StringBuilder builder = new StringBuilder();
+
+            char[] buffer = new char[65536];
+            while (true) {
+                int readByteCount = reader.read(buffer);
+                if (readByteCount == -1) {
+                    break;
+                }
+                builder.append(buffer, 0, readByteCount);
             }
-            sb.append(buffer, 0, readByteCount);
+            reader.close();
+
+            return builder;
         }
-
-        reader.close();
-        processText(sb);
-
-        char[] chars = sb.toString().toCharArray();
-        return new CharArrayReader(chars);
     }
 
     /**
-     * Scans content to find "{{...some-text...}}" and replace it using InteropImpl.
+     * Scans content to find "{{...some-text...}}" and replaces it using InteropImpl.
      *
-     * @param sb Content to be processes.
+     * @param sb content to be processed
      */
-    private static void processText(StringBuffer sb) {
+    private static void processText(StringBuilder sb) {
         int index = 0;
 
         while (index + 1 < sb.length()) {
@@ -73,6 +86,40 @@ public class PreprocessFreemarkerFileTemplateLoader extends FileTemplateLoader {
             }
 
             ++index;
+        }
+    }
+
+    public static void addTemplateSource(String name, String content) {
+        templateSourceByName.put(name, new InmemoryTemplateSource(name, content));
+    }
+
+    public static void addTemplateSource(String name, String content, long modificationTime) {
+        templateSourceByName.put(name, new InmemoryTemplateSource(name, content, modificationTime));
+    }
+
+    @SuppressWarnings("DeserializableClassInSecureContext")
+    private static final class InmemoryTemplateSource extends File {
+        private final String content;
+        private final long modificationTime;
+
+        private InmemoryTemplateSource(String name, String content) {
+            this(name, content, System.currentTimeMillis());
+        }
+
+        private InmemoryTemplateSource(String name, String content, long modificationTime) {
+            super(name);
+            this.content = content;
+            this.modificationTime = modificationTime;
+        }
+
+        private String getContent() {
+            return content;
+        }
+
+        @SuppressWarnings("RefusedBequest")
+        @Override
+        public long lastModified() {
+            return modificationTime;
         }
     }
 }
