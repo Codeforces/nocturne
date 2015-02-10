421edaac6c09adf3d9a88e97b1a8b4066acf61a3
sladethe <sladethe@65389464-0340-11de-9fdb-3debd5008f28>
2015-02-10 13:54:05 +0000

    - added Constants.

---
+++ code/src/main/java/org/nocturne/main/Constants.java
@@ -0,0 +1,17 @@
+package org.nocturne.main;
+
+import freemarker.template.Configuration;
+import freemarker.template.Version;
+
+/**
+ * @author Maxim Shipko (sladethe@gmail.com)
+ *         Date: 10.02.15
+ */
+public class Constants {
+    public static final String CONFIGURATION_FILE = "/nocturne.properties";
+    public static final Version FREEMARKER_VERSION = Configuration.VERSION_2_3_21;
+
+    private Constants() {
+        throw new UnsupportedOperationException();
+    }
+}
